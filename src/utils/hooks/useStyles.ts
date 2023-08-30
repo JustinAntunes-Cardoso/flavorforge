6b97e2c1acd077cab90764a874375c0a67c70a5e
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-08-30 17:24:00 -0400

    Header layout created with respective utils files for data, hook, and type. Footer hook name changed to be more descriptive.

--- src/utils/hooks/useStyles.ts
+++ src/utils/hooks/useStyles.ts
@@ -1,6 +1,58 @@
 import { createStyles, rem } from '@mantine/core';
 
-export const useStyles = createStyles((theme) => ({
+export const useHeaderStyles = createStyles((theme) => ({
+	header: {
+		backgroundColor: theme.fn.variant({
+			variant: 'filled',
+			color: theme.primaryColor,
+		}).background,
+		borderBottom: 0,
+	},
+
+	inner: {
+		height: rem(56),
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+
+	links: {
+		[theme.fn.smallerThan('sm')]: {
+			display: 'none',
+		},
+	},
+
+	burger: {
+		[theme.fn.largerThan('sm')]: {
+			display: 'none',
+		},
+	},
+
+	link: {
+		display: 'block',
+		lineHeight: 1,
+		padding: `${rem(8)} ${rem(12)}`,
+		borderRadius: theme.radius.sm,
+		textDecoration: 'none',
+		color: theme.white,
+		fontSize: theme.fontSizes.sm,
+		fontWeight: 500,
+
+		'&:hover': {
+			backgroundColor: theme.fn.lighten(
+				theme.fn.variant({ variant: 'filled', color: theme.primaryColor })
+					.background!,
+				0.1
+			),
+		},
+	},
+
+	linkLabel: {
+		marginRight: rem(5),
+	},
+}));
+
+export const useFooterStyles = createStyles((theme) => ({
 	footer: {
 		marginTop: rem(120),
 		paddingTop: `calc(${theme.spacing.xl} * 2)`,
