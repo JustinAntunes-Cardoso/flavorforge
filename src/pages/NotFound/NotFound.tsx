4112fe280fb20cea7823edce4173b99b1bba7f57
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-05 23:01:02 -0400

    Fix semantics of the NotFound page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -4,17 +4,17 @@ import _404 from '../../layout/404/404';
 import './NotFound.scss'
 
 //Global data
-import { footerData } from '../../utils/constants/footerLinks';
 import { headerData } from '../../utils/constants/headerSearch';
+import { footerData } from '../../utils/constants/footerLinks';
 
 //Wildcard page if there's an error with URL
 const NotFound = () => {
     return (
-        <main>
+        <>
             <Header links={headerData.links} />
             <_404 />
             <Footer data={footerData.data} />
-        </main>
+        </>
     )
 }
 
