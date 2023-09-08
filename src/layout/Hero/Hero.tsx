9d6ca24adddb9ae3e7eaeb0ad4ae0b155bde6090
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-07 23:04:27 -0400

    Design hero component

--- src/layout/Hero/Hero.tsx
+++ src/layout/Hero/Hero.tsx
@@ -14,11 +14,18 @@ import { IconCheck } from '@tabler/icons-react';
 import veg from '../../assets/icons/vegetables_hero.svg';
 
 const useStyles = createStyles((theme) => ({
+    root: {
+        backgroundColor: theme.fn.variant({
+            variant: 'filled',
+            color: theme.primaryColor,
+        }).background,
+    },
+
     inner: {
         display: 'flex',
         justifyContent: 'space-between',
-        paddingTop: `calc(${theme.spacing.xl} * 4)`,
-        paddingBottom: `calc(${theme.spacing.xl} * 4)`,
+        paddingTop: `calc(${theme.spacing.xl} * 8)`,
+        paddingBottom: `calc(${theme.spacing.xl} * 8)`,
     },
 
     content: {
@@ -32,10 +39,10 @@ const useStyles = createStyles((theme) => ({
     },
 
     title: {
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+        color: theme.colorScheme === 'dark' ? theme.white : theme.white,
         fontFamily: `Greycliff CF, ${theme.fontFamily}`,
         fontSize: rem(44),
-        lineHeight: 1.2,
+        lineHeight: 1.3,
         fontWeight: 900,
 
         [theme.fn.smallerThan('xs')]: {
@@ -43,9 +50,45 @@ const useStyles = createStyles((theme) => ({
         },
     },
 
+    subtitle: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.white,
+        fontSize: rem(22),
+    },
+
+    emoji: {
+        fontSize: rem(36),
+    },
+
+    text: {
+        color: theme.colorScheme === 'dark' ? theme.white : theme.white,
+        fontSize: rem(18),
+    },
+
+    bullet: {
+        backgroundColor: theme.colors.secondary[3],
+        borderRadius: 10,
+        marginTop: rem(1.5),
+
+    },
+
     control: {
         [theme.fn.smallerThan('xs')]: {
             flex: 1,
+        },
+        backgroundColor: theme.colors.secondary[3],
+        '&:hover': {
+            backgroundColor: theme.colors.secondary[5],
+            borderColor: theme.colors.secondary[4],
+        },
+    },
+
+    link: {
+        color: theme.white,
+        backgroundColor: theme.colors.brand[6],
+        borderColor: theme.colors.accents[7],
+        '&:hover': {
+            backgroundColor: theme.colors.accents[3],
+            borderColor: theme.colors.accents[7],
         },
     },
 
@@ -68,16 +111,15 @@ const useStyles = createStyles((theme) => ({
 const Hero = () => {
     const { classes } = useStyles();
     return (
-        <main>
+        <main className={classes.root}>
             <Container>
                 <div className={classes.inner}>
                     <div className={classes.content}>
                         <Title className={classes.title}>
-                            A <span className={classes.highlight}>modern</span> React <br /> components library
+                            <span className={classes.highlight}>Welcome</span> to FlavorForge
                         </Title>
-                        <Text color="dimmed" mt="md">
-                            Build fully functional accessible web applications faster than ever – Mantine includes
-                            more than 120 customizable components and hooks to cover you in any situation
+                        <Text color="dimmed" mt="md" className={classes.subtitle}>
+                            Your Recipe Magic Wand <span className={classes.emoji}>🪄</span>
                         </Text>
 
                         <List
@@ -86,29 +128,27 @@ const Hero = () => {
                             size="sm"
                             icon={
                                 <ThemeIcon size={20} radius="xl">
-                                    <IconCheck size={rem(12)} stroke={1.5} />
+                                    <IconCheck size={rem(15)} stroke={1.5} className={classes.bullet} />
                                 </ThemeIcon>
                             }
+                            className={classes.text}
                         >
                             <List.Item>
-                                <b>TypeScript based</b> – build type safe applications, all components and hooks
-                                export types
+                                <b>Fast & Efficient</b> – Get recipe ideas in seconds, no more wasting time searching for recipes.
                             </List.Item>
                             <List.Item>
-                                <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
-                                any project
+                                <b>Easy to Use</b> – Our user-friendly interface makes meal planning a breeze.
                             </List.Item>
                             <List.Item>
-                                <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
-                                keyboard
+                                <b>Creative Cooking</b> – Discover new recipes and experiment with ingredients you already have.
                             </List.Item>
                         </List>
 
                         <Group mt={30}>
                             <Button radius="xl" size="md" className={classes.control}>
-                                Get started
+                                Get Started
                             </Button>
-                            <Button variant="default" radius="xl" size="md" className={classes.control}>
+                            <Button variant="default" radius="xl" size="md" className={`${classes.control} ${classes.link}`}>
                                 Source code
                             </Button>
                         </Group>
