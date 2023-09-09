c41c4692b65d7cb9560820d469a621f09faa2d9d
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-09 16:49:33 -0400

    Add href global

--- src/utils/constants/globals.ts
+++ src/utils/constants/globals.ts
@@ -1,6 +1,10 @@
-export const links = {
+export const routes = {
 	home: '/',
 	input: '/input',
 	recipeList: '/recipelist',
 	recipe: '/recipe',
+};
+
+export const hrefs = {
+	github: 'https://github.com/JustinAntunes-Cardoso/flavorforge',
 };
