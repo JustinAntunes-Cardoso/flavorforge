1ff6e91c28513f3dab452a18bbf93548470bdc5d
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-09 16:20:33 -0400

    Styles added to button component

---
+++ src/utils/styles/component/useButtonStyles.ts
@@ -0,0 +1,14 @@
+import { createStyles } from '@mantine/core';
+
+export const useButtonStyles = createStyles((theme) => ({
+	control: {
+		[theme.fn.smallerThan('xs')]: {
+			flex: 1,
+		},
+		backgroundColor: theme.colors.secondary[3],
+		'&:hover': {
+			backgroundColor: theme.colors.secondary[5],
+			borderColor: theme.colors.secondary[4],
+		},
+	},
+}));
