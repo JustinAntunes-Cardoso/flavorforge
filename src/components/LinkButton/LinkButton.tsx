c61bba95a77af4b9accf415146165900c5ab7a32
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-10 20:54:08 -0400

    Add conditional prop

--- src/components/LinkButton/LinkButton.tsx
+++ src/components/LinkButton/LinkButton.tsx
@@ -1,18 +1,20 @@
 import { Button } from '@mantine/core';
 import { Link } from 'react-router-dom';
 import useButtonStyles from '../../utils/styles/component/useButtonStyles';
+import { ReactNode } from 'react';
 
 interface LinkButtonProps {
     text: string;
     to: string;
+    leftIcon?: ReactNode;
 }
 
-const LinkButton = ({ text, to }: LinkButtonProps) => {
+const LinkButton = ({ text, to, leftIcon }: LinkButtonProps) => {
     const { classes } = useButtonStyles();
 
     return (
         <Link to={to}>
-            <Button radius="xl" size="md" className={classes.control}>{text}</Button>
+            <Button radius="xl" size="md" className={classes.control} leftIcon={leftIcon ? leftIcon : ''}>{text}</Button>
         </Link>
     );
 };
