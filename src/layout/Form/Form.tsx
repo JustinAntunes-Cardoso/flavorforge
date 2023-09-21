83949ec8ed1da98331c091d68c3e67685001010f
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-21 19:43:07 -0400

    Hot-fix

--- src/layout/Form/Form.tsx
+++ src/layout/Form/Form.tsx
@@ -1,10 +1,10 @@
-import { TagsInput } from '@mantine/core';
+//import { TagsInput } from '@mantine/core';
 
 const Form = () => {
     return (
         <main>
             <form>
-                <TagsInput label="Enter your ingredients" placeholder="Enter food item" splitChars={[',']} aria-label="Multi-Select Ingredients Input" />
+                {/* <TagsInput label="Enter your ingredients" placeholder="Enter food item" splitChars={[',']} aria-label="Multi-Select Ingredients Input" /> */}
             </form>
         </main>
     )
