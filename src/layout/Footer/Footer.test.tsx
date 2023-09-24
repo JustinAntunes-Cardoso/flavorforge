421b8939244d2b1f202b30e9ab5f57054029f358
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-23 22:11:27 -0400

    Refactor App component and tests to Mantine 7.0.0 update

--- src/layout/Footer/Footer.test.tsx
+++ src/layout/Footer/Footer.test.tsx
@@ -5,9 +5,42 @@ import { MantineProvider, MantineThemeOverride } from '@mantine/core';
 
 const mockTheme: MantineThemeOverride = {
     colors: {
-        brand: ['#E65100', '#EF6C00', '#F57C00', '#FB8C00', '#FF9800', '#FFA726', '#FFB74D', '#FFE082', '#FFECB3'],
-        secondary: ['#33691E', '#558B2F', '#689F38', '#7CB342', '#8BC34A', '#9CCC65', '#AED581', '#E6EE9C', '#DCEDC8'],
-        accents: ['#F57F17', '#F9A825', '#FBC02D', '#FBC02D', '#FFEB3B', '#FFEE58', '#FFEE58', '#FFF59D', '#FFF9C4'],
+        brand: [
+            '#E65100',
+            '#EF6C00',
+            '#F57C00',
+            '#FB8C00',
+            '#FF9800',
+            '#FFA726',
+            '#FFB74D',
+            '#FFE082',
+            '#FFECB3',
+            '#FFF6E1',
+        ],
+        secondary: [
+            '#33691E',
+            '#558B2F',
+            '#689F38',
+            '#7CB342',
+            '#8BC34A',
+            '#9CCC65',
+            '#AED581',
+            '#E6EE9C',
+            '#DCEDC8',
+            '#F2FCE8',
+        ],
+        accents: [
+            '#F57F17',
+            '#F9A825',
+            '#FBC02D',
+            '#FBC02D',
+            '#FFEB3B',
+            '#FFEE58',
+            '#FFEE58',
+            '#FFF59D',
+            '#FFF9C4',
+            '#FFFEE0',
+        ],
     },
     primaryColor: 'brand',
 };
@@ -37,7 +70,7 @@ const mockData: FooterLinksProps = {
 describe('Footer Component', () => {
     it('should render the footer text', () => {
         render(
-            <MantineProvider withGlobalStyles withNormalizeCSS theme={mockTheme}>
+            <MantineProvider theme={mockTheme}>
                 <Footer data={mockData.data} />
             </MantineProvider>
         );
@@ -47,7 +80,7 @@ describe('Footer Component', () => {
 
     it('should render the links from props', () => {
         render(
-            <MantineProvider withGlobalStyles withNormalizeCSS theme={mockTheme}>
+            <MantineProvider theme={mockTheme}>
                 <Footer data={mockData.data} />
             </MantineProvider>
         );
