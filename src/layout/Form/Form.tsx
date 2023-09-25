d61a7962f80281c3a6bfbb41ba021624d96018be
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-09-24 22:48:59 -0400

    Handle input logic

--- src/layout/Form/Form.tsx
+++ src/layout/Form/Form.tsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import { TagsInput, Container } from '@mantine/core';
 import LinkButton from '../../components/LinkButton';
 import { routes } from '../../utils/constants/globals';
 
 const Form = () => {
+    const [items, setItems] = useState<string[]>([]);
+
+    const handleItemInputChange = (values: string[]) => setItems([...values]);
+
+    // Serialize the list of items into a string, separated by commas or any other delimiter
+    const serializedItems = items.join(',');
+
+
     return (
         <main>
             <Container>
                 <form>
-                    <TagsInput label="Enter your ingredients" placeholder="Enter food item" splitChars={[',']} aria-label="Multi-Select Ingredients Input" />
-                    <LinkButton to={routes.recipeList} text='Create' />
+                    <TagsInput onChange={handleItemInputChange} label="Enter your ingredients" placeholder="Enter food item" splitChars={[',']} aria-label="Multi-Select Ingredients Input" />
+                    <LinkButton to={`${routes.recipeList}?ingredients=${serializedItems}`} text='Create' />
                 </form>
             </Container>
         </main>
