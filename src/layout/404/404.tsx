ee68da36c385e370949f3ba9fc48d07e5f0f8ad1
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-10-01 20:02:21 -0400

    Refactor 404 styles

--- src/layout/404/404.tsx
+++ src/layout/404/404.tsx
@@ -1,11 +1,10 @@
 import { Container, Title, Text, Group } from '@mantine/core';
 import LinkButton from '../../components/LinkButton';
-import { use404Styles } from '../../utils/styles/layout/use404Styles';
 import { routes } from '../../utils/constants/globals';
 import { _404Image } from './404Icon';
+import classes from './404.module.scss'
 
 const _404 = () => {
-    const { classes } = use404Styles();
 
     return (
         <main className={classes.root}>
@@ -14,12 +13,12 @@ const _404 = () => {
                     <_404Image className={classes.image} />
                     <div className={classes.content}>
                         <Title className={classes.title}>You have found a secret place</Title>
-                        <Text size="lg" align="center" className={classes.description}>
+                        <Text c="dimmed" size="lg" ta="center" className={classes.description}>
                             Unfortunately, this is only a 404 page.
                             You may have mistyped the address, or
                             the page has been moved to another URL
                         </Text>
-                        <Group position="center">
+                        <Group justify="center">
                             <LinkButton to={routes.home} text='Take me back to home page' />
                         </Group>
                     </div>
