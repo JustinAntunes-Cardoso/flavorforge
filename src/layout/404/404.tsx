6ffe1c747c55b0bc47791a996a9b2a6e4fd3d69f
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-10-09 21:26:53 -0400

    Update 404 styles

--- src/layout/404/404.tsx
+++ src/layout/404/404.tsx
@@ -9,20 +9,19 @@ const _404 = () => {
     return (
         <main className={classes.root}>
             <Container>
-                <section className={classes.inner}>
+                <div className={classes.inner}>
                     <_404Image className={classes.image} />
                     <div className={classes.content}>
-                        <Title className={classes.title}>You have found a secret place</Title>
-                        <Text c="dimmed" size="lg" ta="center" className={classes.description}>
-                            Unfortunately, this is only a 404 page.
-                            You may have mistyped the address, or
-                            the page has been moved to another URL
+                        <Title c="white" className={classes.title}>You have found a secret place</Title>
+                        <Text c="white" size="lg" ta="center" className={classes.description}>
+                            Page you are trying to open does not exist. You may have mistyped the address, or the
+                            page has been moved to another URL. If you think this is an error contact support.
                         </Text>
                         <Group justify="center">
                             <LinkButton to={routes.home} text='Take me back to home page' />
                         </Group>
                     </div>
-                </section>
+                </div>
             </Container>
         </main>
     );
