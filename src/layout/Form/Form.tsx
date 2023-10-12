7a7cb9ad95338f1be79104382226eb6744fcfd11
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-10-12 17:43:57 -0400

    Init Form component

--- src/layout/Form/Form.tsx
+++ src/layout/Form/Form.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { TagsInput, Container } from '@mantine/core';
+import { TagsInput, Container, Title } from '@mantine/core';
 import LinkButton from '../../components/LinkButton';
 import { routes } from '../../utils/constants/globals';
+import classes from './Form.module.scss';
 
 const Form = () => {
     const [items, setItems] = useState<string[]>([]);
@@ -13,9 +14,13 @@ const Form = () => {
 
 
     return (
-        <main>
-            <Container>
-                <form>
+        <main className={classes.root}>
+            <Container size="md">
+                <div className={classes.titleContainer}>
+                    <Title c='white' className={classes.title}>Recipe Creator:</Title>
+                    <Title c="white" order={2} className={classes.subtitle}>Enter your ingredients and let us do the magic <span className={classes.emoji}>🧙‍♂️</span></Title>
+                </div>
+                <form className={classes.form}>
                     <TagsInput onChange={handleItemInputChange} label="Enter your ingredients" placeholder="Enter food item" splitChars={[',']} aria-label="Multi-Select Ingredients Input" />
                     <LinkButton to={`${routes.recipeList}?ingredients=${serializedItems}`} text='Create' />
                 </form>
